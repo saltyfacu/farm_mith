c398205b9411f62a3e2082fdba6612bada386674
Facu <fameal@gmail.com>
2021-07-30 11:55:12 +0200

    docs: added comments and moves variables around for clarity

--- scripts/claim_and_sell.py
+++ scripts/claim_and_sell.py
@@ -7,18 +7,39 @@ from brownie.network.gas.strategies import GasNowStrategy
 from datetime import datetime, timedelta
 from time import sleep
 
+# this imports utils.py inside scripts :)
 from .utils import *
 
-gas_strategy = GasNowStrategy('fast')
-gas_ratio = 1
-min_rewards = 0.2
+# let's load the tokens I need
+DAI = interface.ERC20("0x6b175474e89094c44da98b954eedeac495271d0f")
+MIC = interface.ERC20("0x368B3a58B5f49392e5C9E4C998cb0bB966752E51")
+MIS = interface.ERC20("0x4b4D2e899658FB59b1D518b68fe836B100ee8958")
+USDT = interface.ERC20("0xdAC17F958D2ee523a2206206994597C13D831ec7")
 
+# and also the pools where I stake 
+MIC_DAI = interface.MICPOOL("0xcE0058827e6c89E625e524D2fE6E3EF3d9BB6A0c")
+MIC_LINK = interface.MICPOOL("0x0555EEa5f419e18CFc338dEa66aE84Fa7A2fD2BA")
+
+# an array to loop through
 pools = [MIC_DAI, MIC_LINK]
 
+# check gas now and get fast gas price
+gas_strategy = GasNowStrategy('fast')
+
+# just in case I need to exit fast, I use this to multiply gas price from above
+gas_ratio = 1
+
+# I don't want to always sell, at least I need this amount
+min_rewards = 10
+
+
+# the fun part starts here
 def main():
     
+    # which wallet am I going to use?
     owner = the_wallet()
 
+    # this is the status of my wallet
     print(f"The Wallet: {owner}")
     print(f"Balance of USDT: {USDT.balanceOf(owner).to('ether')}")
     print(f"Balance of DAI: {DAI.balanceOf(owner).to('ether')}")
@@ -35,27 +56,40 @@ def main():
     # stake
     MIC_DAI.stake("10_000 ether", {'from': owner, 'gas_limit': 150_000, 'gas_price': gas_price})
 
+    # loop forever and ever... or until CTRL+C whatever happens first
     while(1):
+        
+        # let's check all the pool where I might have staked funds (DAI and LINK)
         for MIC_POOL in pools: 
+            # ask for pending rewards
             pending_reward = MIC_POOL.earned(owner)
             print(f"Pending rewards {pending_reward.to('ether')}, min: {min_rewards}")
 
+            # are rewards more than then min I consider profitable?
             if (pending_reward < (min_rewards * 1e18)):
                 print(f"It's not enough. Less than {min_rewards}. I wait.")
             else:
                 print("Let's claim and sell!")
                 gas_price = gas_strategy.get_gas_price() * gas_ratio
                 
+                # claim the rewards
                 MIC_POOL.getReward({'from': owner, 'gas_limit': 150_000, 'gas_price': gas_price})
                 
+                # I tried to claim, did I succeed?
                 if (MIC.balanceOf(owner) > 0):
+                    # This is my balance
                     print(f"Balance of MIC: {MIC.balanceOf(owner).to('ether')}")
+
+                    # Swap it from USDT using Sushi
                     swapTokensSushi(owner, MIC.balanceOf(owner), MIC, USDT, gas_price)
+                    
+                    # clap! clap! and print balance
                     print("SOLD!")
                     print(f"Balance of USDT: {USDT.balanceOf(owner).to('ether')}")
                 else :
                     print("Coudln't claim rewards")
                 
+                # what's my wallet status?
                 print("")
                 print(f"Balance of USDT: {USDT.balanceOf(owner)/1e6}")
                 print(f"Balance of DAI: {DAI.balanceOf(owner).to('ether')}")
--- scripts/utils.py
+++ scripts/utils.py
@@ -5,16 +5,7 @@ from time import sleep
 
 from brownie import accounts, interface, project, config
 
-#DEBUG = os.environ["DEBUG"] == "1"
-
-DAI = interface.ERC20("0x6b175474e89094c44da98b954eedeac495271d0f")
-MIC = interface.ERC20("0x368B3a58B5f49392e5C9E4C998cb0bB966752E51")
-MIS = interface.ERC20("0x4b4D2e899658FB59b1D518b68fe836B100ee8958")
-USDT = interface.ERC20("0xdAC17F958D2ee523a2206206994597C13D831ec7")
-
-MIC_DAI = interface.MICPOOL("0xcE0058827e6c89E625e524D2fE6E3EF3d9BB6A0c")
-MIC_LINK = interface.MICPOOL("0x0555EEa5f419e18CFc338dEa66aE84Fa7A2fD2BA")
-
+# this is what I will use to swap
 ROUTER = interface.IUniswapV2Router02("0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F") #Sushi
 
 @lru_cache(maxsize=None)
@@ -24,7 +15,7 @@ def all_wallets():
   return wallets
 
 def the_wallet():
-  #return accounts[0]
+  # "force=True" is to take control of a wallet you don't own. It only owkr on mainnet fork
   return accounts.at("0xb527a981e1d415af696936b3174f2d7ac8d11369", force=True)
 
 
@@ -56,16 +47,19 @@ def swapTokensUni(owner, amount, token_in, token_out, gas_price):
 def swapTokensSushi(owner, amount, token_in, token_out, gas_price):
   if token_in.allowance(owner, ROUTER) < amount:
       print("APPROVING...")
+      # allow the contrat to spend my tokens
       token_in.approve(ROUTER, 2**256-1, {'from': owner, 'gas_price': gas_price})
 
+  # usually, the path is TOKEN_A --> WETH --> TOKEN_B
+  # but in this case, I know that the direct path exists
   path = [token_in, token_out]
 
-  # Market buy sell
+  # SWAP!
   return ROUTER.swapExactTokensForTokensSupportingFeeOnTransferTokens( #Sushi
-      amount,
-      0,
-      path,
-      owner,
-      2607999400, #2052
-      {'from': owner, 'gas_price': gas_price, 'gas_limit': 300_000}
+      amount, # amount I send
+      0, # minimum aount I want to receive. I put 0 becasue I'm farming shitcoins and I want to get out of them no matter what
+      path, # the path I set above
+      owner, # who? which wallet?
+      2607999400, #expiration: year 2052, I just don't want it to expire.... maybe it's not the best decision here.
+      {'from': owner, 'gas_price': gas_price, 'gas_limit': 300_000} # for gas_limit, try and set very high in this cases
   )
